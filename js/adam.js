ad4be2e2bb1820a5c551aa3ec6df8bd047ab6224
Adam <at@Adams-MBP.iDom>
2015-09-26 15:02:33 -0600

    added more features to the party

--- js/adam.js
+++ js/adam.js
@@ -1,10 +1,33 @@
-$('.party-btn').click(function(){
-	// $('.party').toggle();
-  $( '.party' ).animate({
-  	// visibility: 'visible'
-  	opacity: '1'
-  	// top: '+= 10'
-  }, 500, function() {
-  	$('body').css('background-color', 'pink');
-  });
-})
+$( document ).ready(function() {
+	    // your code here
+
+	$('.party-btn').click(function(){
+		if($(this).hasClass('not-animated')){
+			startParty();
+		} else {
+			stopParty();
+		}
+	});
+
+	function startParty(){
+		$('.party-btn').removeClass('not-animated');
+		$('.party-btn').addClass('animated');
+		$( '.party' ).animate({
+			opacity: '1'
+		}, 500, function() {
+	  		// $('body').css('background-color', 'pink');
+	  	});
+	}
+
+	function stopParty(){
+		$('.party-btn').removeClass('animated');
+		$('.party-btn').addClass('not-animated');
+		$( '.party' ).animate({
+	  	// visibility: 'visible'
+			opacity: '0'
+	  	// top: '+= 10'
+		}, 500, function() {
+	  		// $('body').css('background-color', 'pink');
+	  	});
+	}
+});
