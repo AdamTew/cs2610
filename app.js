a7f3f4d09f34789e0d03325fedcfec46abd26518
dhoman01 <dustin.e.homan@usu.edu>
2015-10-12 09:13:07 -0600

    Working

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');var express 	= require('express')
+	, exphbs		= require('express-handlebars')
+  , port      = 3000
+  , path      = require('path')
+	, router 	= express.Router()
+	, userRoutes = require('./routes/userRoutes.js')
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', userRoutes)
+
+app.listen(port)
+
+console.log('Server running at http:127.0.0.1:' + port + '/')
