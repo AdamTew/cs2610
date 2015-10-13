3eb3c8ad2d4ba30639dda3b3797b681b9fc332b2
dhoman01 <dustin.e.homan@gmail.com>
2015-10-13 17:16:48 -0600

    Added search hbs and t route

--- public/js/dustin.js
+++ public/js/dustin.js
@@ -7,12 +7,6 @@ function executeSearch() {
   }
   else {
     hideErrorMessage();
-    var noResults = $(".results-pane > h3:first-child");
-    var resultsPaneUl = $(".results-pane > ul");
-    var resultsPane = $(".results-pane");
-    noResults.css("display", "none");
-    resultsPane.css("height", "100%");
-    resultsPaneUl.css("display", "block");
   }
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
 var express = require('express')
-  , router = express.Router();
+  , router = express.Router()
+  , results = require('../results.json');
 
   router.get('/', function(req,res){
     res.render('index', {
       title: "Welcome | Login"
+    })
+  })
+
+  router.get('/search', function(req,res){
+    res.render('search', {
+      results : results,
+      saved: ['awesome', 'spartancoding', 'nodejs'],
+      layout: "auth-base.handlebars"
     })
   })
 
