c824dfa3e86deaa001b1ecdd51adffa1442c012a
dhoman01 <dustin.e.homan@usu.edu>
2015-11-13 09:13:14 -0700

    Pulled user feed from insta

--- routes/dashRoute.js
+++ routes/dashRoute.js
@@ -1,11 +1,26 @@
 var express = require('express')
   , router = express.Router()
-  , results = require('../results.json');
+  , querystring = require('querystring')
+  , request = require('request')
 
 router.get('/dashboard', function(req,res){
-	res.render('dashboard', {
-  		title: "Welcome | Login"
-	})
+  var qs = {
+    access_token: req.session.access_token
+  }
+  var query = querystring.stringify(qs)
+  var url = 'https://api.instagram.com/v1/users/self/feed?'+ query
+
+  var options = {
+    url: url
+  }
+  request(options, function(error, response, body){
+    res.render('dashboard', {
+        title: "Welcome | Login",
+        feedlist: body.data
+    })
+  })
+
+
 })
 
   module.exports = router
